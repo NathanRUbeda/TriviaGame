45eacb79f00978d544eb242787018693dd8312ca
nathanrubeda <ubedanathan@gmail.com>
2024-11-05 13:49:21 -0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Nathan Ubeda on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriviaGameTests/TriviaGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  TriviaGameTests.swift
+//  TriviaGameTests
+//
+//  Created by Nathan Ubeda on 11/5/24.
+//
+
+import Testing
+
+struct TriviaGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
